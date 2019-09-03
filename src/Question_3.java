0edf29742ff74f8b1b98ca23f66718da9743de4b
Flareon19 <aparna.rani_iot18@gla.ac.in>
2019-09-04 01:29:31 +0530

    Create main method Create Scanner object Create the required fields Ques1: Use for loop for the solution Ques2: find Rupees and paise Ques3: Calculate Celsius using given formula

---
+++ src/Question_3.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Question_3 {
+    public static void main() {
+        Scanner scanner = new Scanner(System.in);
+        double Fahrenheit = scanner.nextDouble();
+        double Celsius = (Fahrenheit - 32) / 1.8;
+        System.out.println(Celsius);
+    }
+}
