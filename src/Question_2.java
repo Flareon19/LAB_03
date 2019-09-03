d4a1553d4be4770f0dee46580663ccad570ee4f0
Flareon19 <aparna.rani_iot18@gla.ac.in>
2019-09-04 01:23:03 +0530

    Create main method Create Scanner object Create the required fields Ques1: Use for loop for the solution Ques2: find Rupees and paise

--- src/Question_2.java
+++ src/Question_2.java
@@ -1,3 +1,11 @@
-public class Question_2 {
+import java.util.Scanner;
 
+public class Question_2 {
+    public static void main() {
+        Scanner scanner = new Scanner(System.in);
+        double price = scanner.nextDouble();
+        int Rupees = (int) price;
+        int paisa = (int) (price - Rupees) * 100;
+        System.out.println(Rupees + " Rupees and " + paisa + " Paisa");
+    }
 }
