5a54c91b7482ffff8c0fe1320e5ba324664a87e0
Flareon19 <aparna.rani_iot18@gla.ac.in>
2019-09-04 01:06:38 +0530

    Create main method Create Scanner object Create the required fields

--- src/Queation_1.java
+++ src/Queation_1.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Queation_1 {
     public static void main() {
         Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        double sum = 1.0;
 
 
     }
