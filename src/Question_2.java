e13adfb1104b0d27db17144f4ea94398216f4be7
Flareon19 <aparna.rani_iot18@gla.ac.in>
2019-09-04 01:37:04 +0530

    Create main method Create Scanner object Create the required fields Ques1: Use for loop for the solution Ques2: find Rupees and paise Ques3: Calculate Celsius using given formula

--- src/Question_2.java
+++ src/Question_2.java
@@ -5,7 +5,7 @@ public class Question_2 {
         Scanner scanner = new Scanner(System.in);
         double price = scanner.nextDouble();
         int Rupees = (int) price;
-        int paisa = (int) (price - Rupees) * 100;
+        int paisa = (int) ((price - Rupees) * 100);
         System.out.println(Rupees + " Rupees and " + paisa + " Paise");
     }
 }
